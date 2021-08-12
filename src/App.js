019f3f4767f1111293071852fe890bfe603dcb6c
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2021-08-12 14:16:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = () => {
         <div className={classes.footer}>
           <Typography variant="body1" component="h2">
             Created by
-            <a className={classes.link} href="https://krishshah.tech/" target="_blank"> Krish Shah</a> 
+            <a className={classes.link} href="https://krish-shah.vercel.app" target="_blank"> Krish Shah</a> 
           </Typography>
         </div>
       ) : null}
